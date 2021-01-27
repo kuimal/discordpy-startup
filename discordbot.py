583d70a74119d965b1375825795f3224def8a64d
kuimal <78090101+kuimal@users.noreply.github.com>
2021-01-27 23:25:47 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -16,6 +16,21 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
-
+    
+# BOT動作プログラム
+@client.event
+# async defでコルーチン宣言
+# on_messageメッセージがサーバーに送信されたとき呼び出される。
+async def on_message(message):
+ # messageが「おはよう」で始まるか?
+ if message.content.startswith("どすこい"):
+  # 送り主がBotだった場合反応しないように条件つける。
+  if client.user != message.author:
+   # メッセージをmに格納。
+   # message.author.nameに送り主の名前が入る。
+   m = message.author.name + "うるせえ"
+   # awaitはコルーチン内部でコルーチンを宣言する時に。
+   # メッセージが送られてきたチャンネルへメッセージを送ります
+   await client.send_message(message.channel, m)
 
 bot.run(token)
